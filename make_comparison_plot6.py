3ef65dd62f764d528e87b8eed08a3f469b45fa5e
Peilong Wang <peilong.wang@cern.ch>
2018-02-16 13:03:42 +0100

    added make_comparison_plot6.py

--- make_comparison_plot6.py
+++ make_comparison_plot6.py
@@ -19,18 +19,37 @@ def getHistogram(oneHistName):
     return hists
 
 
+def getFracError(histName):
+    if "2tag2jet_150ptv_" in histName:    
+        print "peilong: debug ---------------->2tag2jet_150ptv"
+        return 0.02
+    if "2tag2jet_75_150ptv_" in histName:
+        print "peilong: debug ---------------->2tag2jet_75_150ptv"
+        return 0.01
+    if "2tag3jet_150ptv_" in histName:
+        print "peilong: debug ---------------->2tag3jet_150ptv"
+        return 0.01
+    if "2tag3jet_75_150ptv_" in histName:
+        print "peilong: debug -----------------!!!------------->2tag3jet_75_150ptv, 0.005"
+        return 0.005
+
+
+
 def getMergeBins(hist):
-    FracError = 0.01
+    FracError = getFracError(hist.GetName())
+
     integral = 0.0
     error2 = 0.0
     Nbins = hist.GetNbinsX()
     startbin = Nbins
     currentbin = Nbins
-    binedges = [hist.GetBinCenter(Nbins) + hist.GetBinWidth(Nbins)]
+    binedges = [hist.GetBinCenter(Nbins) + hist.GetBinWidth(Nbins) / 2]
 
     for iBin in range (Nbins,0,-1):
+    # for iBin in range (0,Nbins,1):
         integral += hist.GetBinContent(iBin)
         error2 += hist.GetBinError(iBin)**2
+        # print "integral == ", integral
         if (integral == 0 or error2 == 0): continue
         if iBin == 1:
             binedges.append(hist.GetBinCenter(iBin) - hist.GetBinWidth(iBin) / 2)
@@ -44,34 +63,64 @@ def getMergeBins(hist):
     return binedges
 
 
-def getRangeFromHist(hist):
-    name = hist.GetName()
-    xmin = 0
-    xmax = 500
-    if "2jet" in name:
-        if "mBB" in name:
+def getXRangeFromHist(hist):
+    nOfBin = hist.GetNbinsX()
+    if nOfBin == 1:
+        lower = 0
+        upper = 1000
+
+    elif nOfBin >= 1:
+        name = hist.GetName()
+        # xmin = 0
+        # xmax = 500
+
+        if "_mBB_" in name:
+            xmin = 0
             xmax = 450
-        elif "pTV" in name:
-            xmin = 150
+        if "_pTV_" in name:
+            xmin = 75
+            xmax = 300
+        if "_MET_" in name:
+            xmin = 0
+            xmax = 300
+        if "_dPhiVBB_" in name:
+            xmin = 2.5
+            xmax = 3.14
+        if "_dYWH_" in name:
+            xmin = 0
+            xmax = 2.90
+        if "_Mtop_" in name:
+            xmin = 100
+            xmax = 240
+        if "_mTW_" in name:
+            xmin = 0
+            xmax = 300
+        if "_pTB1_" in name:
+            xmin = 20
             xmax = 400
-        elif "dRBB" in name:
-            xmax = 6
-    elif "3jet" in name:
-        if "mBB" in name:
-            xmax = 450
-        elif "pTV" in name:
-            xmin = 150
-            xmax = 450
-        elif "dRBB" in name:
-            xmax = 6
+        if "_pTB2_" in name:
+            xmin = 20
+            xmax = 400
 
-    upperbin = hist.GetXaxis().FindBin(xmax)
-    upper = hist.GetBinCenter(upperbin) + hist.GetBinWidth(upperbin) / 2
+        upperbin = hist.GetXaxis().FindBin(xmax) - 1
+        print "peilong: upperbin number ", upperbin
+        lowerbin = hist.GetXaxis().FindBin(xmin) + 1
+        print "peilong: lowerbin number ", lowerbin
 
-    lowerbin = hist.GetXaxis().FindBin(xmin)
-    lower = hist.GetBinCenter(lowerbin) - hist.GetBinWidth(lowerbin) / 2
+    return lowerbin, upperbin
+            
+    #     upperbin = hist.GetXaxis().FindBin(xmax)
+    #     print "peilong: upperbin number ", upperbin
+    #     upper = hist.GetBinCenter(upperbin) - hist.GetBinWidth(upperbin) / 2
+    #     print "peilong: upper ", upper
 
-    return lower,upper
+    #     if xmin != 0:
+    #         lowerbin = hist.GetXaxis().FindBin(xmin)
+    #         lower = hist.GetBinCenter(lowerbin) + hist.GetBinWidth(lowerbin) / 2
+    #     else:
+    #         lower = 0
+
+    # return lower,upper
 
 
 def makeComparisonHistogram(oneHistName, histograms):
@@ -85,7 +134,7 @@ def makeComparisonHistogram(oneHistName, histograms):
 
     c1 = TCanvas( 'c1', 'Example',700, 500 )
     colors = [2,1,3,4,6]
-    leg = TLegend(0.6, 0.6, 0.9, 0.9)
+    leg = TLegend(0.7, 0.71, 0.9, 0.91)
     sampleName=["aMc@NloPythia8EvtGen_A14N23LO_ttbar 410225", "PowhegPythia8EvtGen_A14_ttbar_hdamp258p75 (nominal)", "PowhegPythia8EvtGen_ttbar_hdamp517p5 410511", "PowhegPythia8EvtGen_ttbar_hdamp258p75 410512", "PowhegHerwig7EvtGen_H7UE_tt_hdamp258p75 410525"]
 
     pad1 = TPad("pad1", "up", 0., 0.5, 1., 1.)
@@ -110,19 +159,31 @@ def makeComparisonHistogram(oneHistName, histograms):
     binnum=len(bins)-1
     edge = array('d', bins)
 
-    # get x-axis range for the histograms
-    xmin,xmax = getRangeFromHist(histograms[0])
-
     # plot shape comparison
     hNewHists = []
 
     for i, h in enumerate(histograms):
         hNew = h.Rebin(binnum,h.GetName(),edge)
+        for bin in xrange(0,hNew.GetNbinsX()+1):
+            hNew.SetBinContent(bin,hNew.GetBinContent(bin)/(hNew.GetBinWidth(bin)) ) 
+            hNew.SetBinError(bin,hNew.GetBinError(bin)/(hNew.GetBinWidth(bin)) ) 
+
         hNew.SetLineColor(colors[i])
         hNew.SetMarkerColor(colors[i])
         hNew.Scale(1/hNew.Integral())
-        print "xmin, xmax: ", xmin, xmax
-        hNew.GetXaxis().SetRangeUser(xmin, xmax)
+        # get x-axis range for the histograms
+        physValueName = ["_mBB_", "_pTV_", "_MET_", "_dPhiVBB_", "_dYWH_", "_Mtop_", "_mTW_", "_pTB1_", "_pTB2_"]
+        if "_WhfSR_" in oneHistName:
+            if any(value in oneHistName for value in physValueName):
+                xmin,xmax = getXRangeFromHist(hNew)
+                print "xmin, xmax: ", xmin, xmax
+                # hNew.GetXaxis().SetRangeUser(xmin, xmax)
+                # 
+                hNew.GetXaxis().SetRange(xmin, xmax)
+        # get x-axis range for the histograms
+        ymax = 1.4 * hNew.GetMaximum() 
+        hNew.GetYaxis().SetRangeUser(0, ymax)
+
         # histNameTemp = hNew.GetName()
         # print histNameTemp
         # if "WhfSR_mBB" in histNameTemp:
@@ -155,26 +216,21 @@ def makeComparisonHistogram(oneHistName, histograms):
     # make ratio plot
     # hNewHists are copied from the last step; the settings from the last step are also copied
     for i, h in enumerate(hNewHists):
-        hNewHists[i].GetYaxis().SetTitle("ratio")
-        hNewHists[i].GetYaxis().SetRangeUser(0, 1.9)
-        option="" if i==0 else "same"
+        # hNewHists[i].GetYaxis().SetTitle("ratio")
+        # hNewHists[i].GetYaxis().SetRangeUser(0.7, 1.65)
+
+        # h.GetYaxis().SetTitle("ratio")
         if i != 1: # skip the norminal
             h.Divide(hNewHists[1])
+            h.GetYaxis().SetTitle("ratio")
+            # ratioYMax = 1.2 * h.GetMaximum() 
+            # ratioYMin = 1.2 * h.GetMinimum() 
+            # h.GetYaxis().SetRangeUser(ratioYMin, ratioYMax)
+
+            h.GetYaxis().SetRangeUser(0.8, 1.31)
+
+            option="" if i==0 else "same"
             h.Draw(option)
-
-
-    # ratiosOld = []
-
-    # for i, h in enumerate(histograms):
-    #     ratiosOld.append(h.Clone())
-
-    #     ratiosOld[i].Divide(histograms[1])
-    #     # ratiosOld[i].SetTitle("")
-    #     ratiosOld[i].GetYaxis().SetTitle("ratio")
-    #     ratiosOld[i].GetYaxis().SetRangeUser(0.6, 1.55)
-    #     option="" if i==0 else "same"
-    #     if i != 1: # skip the norminal
-    #         ratiosOld[i].Draw(option)
 
 
     # define fit fuction for mBB and pTV from EPS 2017
@@ -230,7 +286,7 @@ def makeComparisonHistogram(oneHistName, histograms):
 
 
     # plot EPS shape
-    if "mBB" in oneHistName:
+    if "_mBB_" in oneHistName:
         func=TF1("f_Systtbar_mBB_1lep", fitFunctionmBB, 0, 1000)
         func.SetParameter(0, 0.917)
         func.SetParameter(1, 0.389)
@@ -239,7 +295,7 @@ def makeComparisonHistogram(oneHistName, histograms):
         leg.AddEntry(func,"EPS")
         func.Draw('same')
 
-    if "pTV" in oneHistName:
+    if "_pTV_" in oneHistName:
         func=TF1("f_SysTTbarPTV_1lep", fitFunctionpTV, 0, 1000)
         # the following are the initialization
         func.SetParameter(0, 1.09)
